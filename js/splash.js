f6473b11d71f52816f249a567691bc0ef8f24c0e
Hugo Vidal <hugovima09@gmail.com>
2026-04-27 18:49:22 +0200

    add about me, change border radious for border bottom in contact

--- js/splash.js
+++ js/splash.js
@@ -1,6 +1,5 @@
 window.addEventListener("load", function() {
 	setTimeout(function() {
 		document.getElementById("splash").classList.add("fade-out");
-		document.getElementById("content").style.display = "block";
 	}, 2500); // duración total del splash
 });
