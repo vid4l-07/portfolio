b394c4694b811c366781f7c86cb54cbadff8f649
Hugo Vidal <hugovima09@gmail.com>
2026-06-20 19:10:40 +0200

    create svgs section, optimize stars.js

--- js/stars.js
+++ js/stars.js
@@ -1,20 +1,27 @@
+fetch("https://api.github.com/users/vid4l-07/repos?per_page=100")
+	.then(res => res.json())
+	.then(repos => {
+		const map = {};
 
-fetch("https://api.github.com/repos/vid4l-07/Klip")
-  .then(res => res.json())
-  .then(data => {
-    document.getElementById("klip-stars").textContent = data.stargazers_count;
-  })
-  .catch(() => {
-    document.getElementById("klip-stars").textContent = "?";
-  });
+		repos.forEach(repo => {
+			map[repo.name] = repo.stargazers_count;
+		});
+	
+		if (map["Klip"] == null) {
+			document.getElementById("klip-stars-org").style.display = "none";
+		} else {
+			document.getElementById("klip-stars").textContent = map["Klip"];
+		}
+
+		if (map["dotfiles-hyprland"] == null) {
+			document.getElementById("hypr-stars-org").style.display = "none";
+		} else {
+			document.getElementById("hypr-stars").textContent = map["dotfiles-hyprland"];
+		}
 
 
-fetch("https://api.github.com/repos/vid4l-07/dotfiles-hyprland")
-  .then(res => res.json())
-  .then(data => {
-    document.getElementById("hypr-stars").textContent = data.stargazers_count;
-  })
-  .catch(() => {
-    document.getElementById("hypr-stars").textContent = "?";
-  });
-
+	})
+	.catch(() => {
+		document.getElementById("klip-stars-org").style.display = "none";
+		document.getElementById("hypr-stars-org").style.display = "none";
+	});
